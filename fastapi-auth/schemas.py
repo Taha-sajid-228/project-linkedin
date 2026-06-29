62d213c1f6858b7611cbfe284ed7e97a02c35886
Taha-sajid-228 <jerrytaha7961@gmail.com>
2026-06-29 18:28:54 +0500

    Complete OTP timer and admin route protection

--- fastapi-auth/schemas.py
+++ fastapi-auth/schemas.py
@@ -1,5 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
 
 class UserCreate(BaseModel):
-    name: str
-    email: str
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    name: Optional[str] = None
+    role: str
+    is_verified: bool
+    provider: str
+    profile_picture: Optional[str] = None
+
+    class Config:
+        from_attributes = True

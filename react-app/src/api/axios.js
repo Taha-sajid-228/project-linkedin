aed37311141eff95842f6294d065d1ba3e2a90ef
Taha-sajid-228 <jerrytaha7961@gmail.com>
2026-07-15 13:59:49 +0500

    Prepare frontend for Vercel deployment

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -1,15 +1,17 @@
 import axios from "axios";
-
+console.log(import.meta.env.VITE_API_URL);
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => {
